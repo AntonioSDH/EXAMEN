b1b4f06c22af153bf7fa0f664be60cdfe20042af
AntonioSDH <113518700+AntonioSDH@users.noreply.github.com>
2022-11-22 17:07:37 +0100

    Add files via upload

---
+++ creapelis.php
@@ -0,0 +1,31 @@
+<?php
+$servidor= "localhost";
+$usuario= "EXAMEN";
+$password = "12345";
+$nombreBD= "db_peliculas";
+
+// Creamos la conexión con el servidor SQL
+$conn = mysqli_connect($servidor, $usuario, $password, $nombreBD);
+// Verificamos la conexión con el servidor MySQL
+if ($conn->connect_error) {
+    die("la conexión ha fallado: " . $conn->connect_error);
+}
+
+// Crea la tabla
+$sql = "CREATE TABLE PELICULAS (
+id INT(10),
+id_director INT(10),
+titulo VARCHAR(300),
+genero VARCHAR(64),
+precio INT(8),
+)";
+
+// Se verifica si la tabla ha sido creado
+if ($conn->query($sql) === TRUE) {
+    echo "la tabla peliculas ha sido creado";
+} else {
+    echo "Hubo un error al crear la tabla peliculas: " . $conn->error;
+}
+// Cerramos la conexión
+$conn->close();
+?>
