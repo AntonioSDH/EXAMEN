3f764b3c91011e04f74dd3e623de33e875c50aca
AntonioSDH <113518700+AntonioSDH@users.noreply.github.com>
2022-11-22 17:48:32 +0100

    Update intropelis.php

--- intropelis.php
+++ intropelis.php
@@ -1,4 +1,8 @@
 <?php
+$servidor= "localhost";
+$usuario= "EXAMEN";
+$password = "12345";
+$nombreBD= "db_peliculas";
 // Creamos la conexión con la base de datos
 $conn = mysqli_connect($servidor, $usuario, $password, $nombreBD);
 // Aquí se revisa la conexión con MySQL
@@ -7,14 +11,14 @@ if (!$conn) {
 }
 
 // La variable que creara la tabla en la base de datos.
-$mi_tabla= "INSERT INTO peliculas VALUES (1,1,'Hell Boy','Accion','20'),(1,1,'Hell Boy 1','Accion','20'),(1,1,'Hell Boy','Accion','20')";
+$mi_tabla= "INSERT INTO peliculas VALUES (1,1,'Hell Boy','Accion','20'),(2,2,'WRC','Accion','20'),(3,3,'Spiderman','Accion','20')";
 // Condicional PHP que creará la tabla
 if (mysqli_query($conn, $mi_tabla)) {
-// Mostramos mensaje si la tabla ha sido creado correctamente!
-    echo "Table MyGuests created successfully";
+// Mostramos mensaje si la tabla ha sido actualizada correctamente!
+    echo "Tabla acualizada correctamente";
 } else {
     // Mostramos mensaje si hubo algún error en el proceso de creación
-    echo "Error al crear la tabla: " . mysqli_error($conn);
+    echo "Error al actualizar la tabla: " . mysqli_error($conn);
 }
 // Cerramos la conexión
 mysqli_close($conn);
